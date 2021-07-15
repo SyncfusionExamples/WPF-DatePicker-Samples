d04a22ad7c3a21d3056bcfa483a4d17979baf0d7
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2021-07-15 10:31:20 +0530

    Added sample and FT link for WPF DatePicker keyword

---
+++ SfDatePicker_ArrowKeySample/SfDatePicker_ArrowKeySample/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Syncfusion.Windows.Controls;
+using Syncfusion.Windows.Controls.Input;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SfDatePicker_ArrowKeySample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // SfDateSelector
+        SfDateSelector selector = null;
+
+        // Popup
+        Popup popUp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MonthDatePicker.Loaded += MonthDatePicker_Loaded;                        
+        }
+
+        
+
+        /// <summary>
+        /// Invoked when SfDatePicker Loaded
+        /// </summary>
+        private void MonthDatePicker_Loaded(object sender, RoutedEventArgs e)
+        {
+            popUp = MonthDatePicker.Template.FindName("PART_DropDown", MonthDatePicker) as Popup;
+            selector = MonthDatePicker.Template.FindName("PART_DateSelector", MonthDatePicker) as SfDateSelector;
+            if (popUp != null)
+            {
+                popUp.Opened += PopUp_Opened;
+            }
+        }
+
+        /// <summary>
+        /// Invoked when the popup opened
+        /// </summary>
+        private void PopUp_Opened(object sender, EventArgs e)
+        {
+            Keyboard.Focus(selector);
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-Read me file
+# WPF DatePicker Samples
+
+Repository contains various WPF DatePicker (SfDatePicker) samples.
+
+1. [How to change the date using up or down navigation keys in WPF DatePicker (SfDatePicker)?](https://www.syncfusion.com/kb/9345/how-to-change-the-date-using-up-down-navigation-keys-in-sfdatepicker) - Demo
+2. [How to display more looping items in the DateSelector and TimeSelector?](https://www.syncfusion.com/kb/8758/how-to-display-more-looping-items-in-the-dateselector-and-timeselector) - Demo
+3. [How to change the background color for LoopingSelectorItem in WPF DatePicker (SfDatePicker)?](https://www.syncfusion.com/kb/8228/how-to-change-the-background-color-for-loopingselectoritem-in-sfdatepicker) - Demo
